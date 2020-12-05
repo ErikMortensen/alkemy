a06a923a41297aa93970451c492f068f3ea398ca
Erik <mortensenerikm@gmail.com>
2020-12-04 21:41:43 -0300

    User factory and seeder

--- app/User.php
+++ app/User.php
@@ -10,13 +10,15 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const USER_DEVELOPER = 'developer';
+    const USER_CUSTOMER = 'customer';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'role', 'email', 'password',
     ];
 
     /**
@@ -36,4 +38,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isDeveloper(){
+        return $this->role == User::USER_DEVELOPER;
+    }
+
+    public function isCustomer(){
+        return $this->role == User::USER_CUSTOMER;
+    }
 }
